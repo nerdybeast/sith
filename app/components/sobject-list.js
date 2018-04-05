4a1241f4ffe44f7681c91a39d0596e2c63d005e2
Michael Penrod <therealnerdybeast@gmail.com>
2018-04-05 17:14:01 -0500

    Added filter to sobject list.

--- app/components/sobject-list.js
+++ app/components/sobject-list.js
@@ -1,7 +1,20 @@
 import Component from '@ember/component';
 import SimpleBar from 'simplebar';
+import { isBlank } from '@ember/utils';
 
 export default Component.extend({
+
+	filterMatch: null,
+	filteredSobjects: null,
+
+	init() {
+
+		this._super(...arguments);
+
+		if(this.get('filteredSobjects') === null) {
+			this.set('filteredSobjects', this.get('sobjects'));
+		}
+	},
 
 	didInsertElement() {
 		
@@ -15,6 +28,24 @@ export default Component.extend({
 
 		setAsActiveSobject(sobjectId) {
 			this.get('onClick')(sobjectId);
+		},
+
+		filterList() {
+
+			const filterMatch = (this.get('filterMatch') || '').trim().toLowerCase();
+			const sobjects = this.get('sobjects');
+
+			if(isBlank(filterMatch)) {
+				this.set('filteredSobjects', sobjects);
+				return;
+			}
+			
+			const matchingObjects = sobjects.filter(sobject => {
+				const { name, label } = sobject.getProperties('name', 'label');
+				return name.toLowerCase().includes(filterMatch) || label.toLowerCase().includes(filterMatch);
+			})
+
+			this.set('filteredSobjects', matchingObjects);
 		}
 
 	}
